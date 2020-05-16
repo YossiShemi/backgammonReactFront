45934286884434c6dc652ecf15e477dc1ef02819
Yossi Shemi <yossi20941@gmail.com>
2020-05-16 22:10:00 +0300

    Updates

---
+++ precache-manifest.0529c25f521bd41f3801d1f08271e579.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a8630cfee8f5cfbb8438ca68a0b7e79",
+    "url": "/backgammonReactFront/index.html"
+  },
+  {
+    "revision": "9a6919387b8cf2c3d2bc",
+    "url": "/backgammonReactFront/static/css/main.1e3d4316.chunk.css"
+  },
+  {
+    "revision": "b5b477bfdd17d2e29960",
+    "url": "/backgammonReactFront/static/js/2.ed21f784.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/backgammonReactFront/static/js/2.ed21f784.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a6919387b8cf2c3d2bc",
+    "url": "/backgammonReactFront/static/js/main.8782addd.chunk.js"
+  },
+  {
+    "revision": "67a8808fc0911b7e9d7e",
+    "url": "/backgammonReactFront/static/js/runtime-main.a6474a1a.js"
+  },
+  {
+    "revision": "18606d77182d235c95586bde20b1dcbd",
+    "url": "/backgammonReactFront/static/media/winnerback.18606d77.jpg"
+  }
+]);
